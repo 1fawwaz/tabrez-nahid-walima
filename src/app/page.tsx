5843ece34103454aa9066ece47fc832ebe26beca
1fawwaz <fawwazwaghoo8@gmail.com>
2026-09-23 13:43:03 +0530

    Keep flower-final-tl at top: -40px, left: -25px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,7 +231,7 @@ export default function Home() {
               src="/images/flower-final-tl.png"
               alt=""
               className="absolute w-[58%] max-w-[265px] h-auto origin-top-left flower-sway-e pointer-events-none select-none"
-              style={{ top: "-50px", left: "0px" }}
+              style={{ top: "-40px", left: "-25px" }}
               draggable={false}
             />
             <img
